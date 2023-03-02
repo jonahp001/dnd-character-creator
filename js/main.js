c1778f9a85b0b72830d83647fac6b26dfd47ce30
Jonah Park <jonahp1234@gmail.com>
2023-03-02 23:16:36 +0000

    issue 2: select a race show/ hide icons

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ var $homePage = document.querySelector('#home-page');
 var $characterCreationPage = document.querySelector('#character-creation-page');
 var $h1TitleText = document.querySelector('h1');
 
+var $selectARace = document.querySelector('#select-a-race');
+var $raceCaret = document.querySelector('#race-caret');
+var $raceIcons = document.querySelector('#race-icons');
+
 $startButton.addEventListener('click', function (event) {
   $homePage.setAttribute('class', 'hidden');
   $characterCreationPage.setAttribute('class', '');
@@ -12,4 +16,16 @@ $startButton.addEventListener('click', function (event) {
 $h1TitleText.addEventListener('click', function (event) {
   $homePage.setAttribute('class', '');
   $characterCreationPage.setAttribute('class', 'hidden');
+});
+
+$selectARace.addEventListener('click', function (event) {
+  if (event.target.getAttribute('class') === 'character-h2') {
+    $selectARace.setAttribute('class', 'character-h2-expanded');
+    $raceCaret.setAttribute('class', 'fa-solid fa-caret-down');
+    $raceIcons.setAttribute('class', 'pic-section');
+  } else if (event.target.getAttribute('class') === 'character-h2-expanded') {
+    $selectARace.setAttribute('class', 'character-h2');
+    $raceCaret.setAttribute('class', 'fa-solid fa-caret-left');
+    $raceIcons.setAttribute('class', 'pic-section hidden');
+  }
 });
